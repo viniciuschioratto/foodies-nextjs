c3418028717e1f3c71a258476b1b25061bfb54b2
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2024-03-19 17:36:47 -0300

    feat: Foodies project

---
+++ components/main-header/main-header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import classes from "./main-header.module.css";
+
+import logoImg from "@/assets/logo.png";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="A plate with food on it" priority="true" />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
